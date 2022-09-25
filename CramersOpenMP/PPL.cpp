30efc642046ce32a9eabada7bddd82d5904091b3
Yung How <81214607+TeeYH12345@users.noreply.github.com>
2022-09-25 13:18:16 +0800

    Add files via upload

--- CramersOpenMP/PPL.cpp
+++ CramersOpenMP/PPL.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <omp.h>
 #include <ppl.h>
+#include "./color.hpp"
+
 
 using namespace std;
 using namespace concurrency;
@@ -194,31 +196,41 @@ std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) {
 int main() {
     double start_time, end_time;
     std::vector<std::vector<double>> equations = {
-        { 2, -1,  5,  1,  -3},
-        { 3,  2,  2, -6, -32},
-        { 1,  3,  3, -1, -47},
-        { 5, -2, -3,  3,  49},
+        {1 ,7 ,2 ,3 ,9 ,5 ,7 ,5 ,9 ,8},
+        {4 ,1 ,2 ,2 ,5 ,9 ,1 ,2 ,7 ,5},
+        {4 ,9 ,7 ,4 ,8 ,5 ,3 ,4 ,5 ,3 },
+        {3 ,7 ,5 ,7 ,9 ,2 ,1 ,1 ,6 ,8},
+        {2 ,3 ,2 ,5 ,6 ,5 ,4 ,4 ,3 ,3},
+        {1 ,2 ,1 ,1 ,7 ,7 ,4 ,3 ,5 ,1},
+        {3 ,6 ,8 ,6 ,2 ,5 ,3 ,3 ,2 ,1},
+        {1 ,6 ,5 ,5 ,1 ,9 ,3 ,7 ,2 ,7},
+        {3 ,5 ,8 ,3 ,6 ,5 ,5 ,4 ,5 ,1 },
     };
 
     /*for (int i = 0; i < 1000; i++){
         auto solution = solveCramerSerial(equations);
         solution = solveCramer(equations);
     }*/
-        start_time = omp_get_wtime();
-        auto solution = solveCramerSerial(equations);
-        end_time = omp_get_wtime() - start_time;
+    start_time = omp_get_wtime();
+    auto solution = solveCramerSerial(equations);
+    end_time = omp_get_wtime() - start_time;
+    double serial_time = end_time;
 
 
+    std::cout << "Serial time taken in seconds: " << dye::green(end_time) << dye::green("s\n");
+    std::cout << solution << '\n';
 
-        std::cout << "Serial time taken in seconds: " << end_time << "s\n";
-        std::cout << solution << '\n';
+    start_time = omp_get_wtime();
+    solution = solveCramer(equations);
+    end_time = omp_get_wtime() - start_time;
 
-        start_time = omp_get_wtime();
-        solution = solveCramer(equations);
-        end_time = omp_get_wtime() - start_time;
+    std::cout << "Parallel time taken in seconds: " << dye::green(end_time) << dye::green("s\n");
+    std::cout << solution << '\n' << std::endl;
 
-        std::cout << "Parallel time taken in seconds: " << end_time << "s\n";
-        std::cout << solution << '\n';
+    if (end_time > serial_time)
+        std::cout << "Serial is faster than parallel by " << dye::green(end_time - serial_time) << dye::green("s\n");
+    else
+        std::cout << "Parallel is faster than serial by " << dye::green(serial_time - end_time) << dye::green("s\n");
     
     return 0;
 }
